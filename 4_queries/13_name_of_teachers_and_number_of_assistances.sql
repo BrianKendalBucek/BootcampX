ff6bc7bda7b2e4cb61c5d8ed00667835ce2b1405
BrianKendalBucek <bbucek@gmail.com>
2022-09-24 21:48:40 +0000

    more queries added

---
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,6 @@
+assignment_submissions (id, assignment_id, student_id, submission_date, duration)
+assignments (id, name, content, duration, day, chapter)
+assistance_requests (id, student_id, teacher_id, assignment_id, created_at, started_at, completed_at, student_feedback, teacher_feedback)
+cohorts (id, name, start_date, end_date)
+students (id, name, email, phone, github, start_date, end_date, cohort_id)
+teachers (id, name, is_active, start_date, end_date)
