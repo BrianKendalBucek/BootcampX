d1c46ad40411203968f622e05f19fdffc551b1f7
BrianKendalBucek <bbucek@gmail.com>
2022-09-25 04:46:58 +0000

    added query 13

--- 4_queries/13_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -1,6 +1,8 @@
-assignment_submissions (id, assignment_id, student_id, submission_date, duration)
-assignments (id, name, content, duration, day, chapter)
-assistance_requests (id, student_id, teacher_id, assignment_id, created_at, started_at, completed_at, student_feedback, teacher_feedback)
-cohorts (id, name, start_date, end_date)
-students (id, name, email, phone, github, start_date, end_date, cohort_id)
-teachers (id, name, is_active, start_date, end_date)
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
